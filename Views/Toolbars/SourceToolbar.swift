753359a721eca6bf521bde497f2dfd55179a03d3
Tobias (iPad Pro) <mailme@tobiaspott.de>
2023-12-30 03:15:20 +0100

    Added zoom lock back to source toolbar

--- Views/Toolbars/SourceToolbar.swift
+++ Views/Toolbars/SourceToolbar.swift
@@ -8,12 +8,13 @@ struct SourceToolbar: View {
     var body: some View {
             
             
-            RoundedStateButton(systemName: "magnifyingglass", action: { state.srcZoomLocked.toggle(); }, state: state.srcZoomLocked, stateColor: .gray, background: Styling.buttonColor, padding: 10.0)
-            
         VStack(content: {
             RoundedPanel(content: {
                 RoundedButton(systemName: "viewfinder.circle", action: { state.drag.location = CGPoint.zero; state.drag.fixedLocation = CGPoint.zero })
                 RoundedStateButton(systemName: "arrow.up.and.down.and.arrow.left.and.right", action: { state.srcDragLocked.toggle(); }, state: state.srcDragLocked, stateColor: .gray, background: Styling.buttonColor)
+                
+                RoundedStateButton(systemName: "magnifyingglass", action: { state.srcZoomLocked.toggle(); }, state: state.srcZoomLocked, stateColor: .gray, background: Styling.buttonColor, padding: 10.0)
+                
             }, orientation: .vertical)
         })        
     }
