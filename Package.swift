c6b8281d92ffa4cba26663d9e14322268854e9dc
Tobias (iPad Pro) <mailme@tobiaspott.de>
2024-01-22 16:38:54 +0100

    Fixed missing splash screen

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             bundleIdentifier: "de.tobiaspott.playground.brickartinstructor",
             teamIdentifier: "LR2W97LX43",
             displayVersion: "0.13.2",
-            bundleVersion: "89",
+            bundleVersion: "90",
             appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [
